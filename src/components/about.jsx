6294d5ac14a42014feb7a6020cfb4a54a976361b
Oxygeeeen <abdulbaasitisah436@gmail.com>
2023-12-23 15:25:59 +0100

    change favicon and profile-pic

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -33,7 +33,7 @@ export function About() {
             boxShadow="lg"
             size="xl"
             name="Abdulbaasit Isah"
-            src="https://images.unsplash.com/photo-1611432579699-484f7990b127?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+            src="assets/profile.jpeg"
           />
           <Stack spacing="1">
             <Heading as="h1" size="lg" color="blue.600">
